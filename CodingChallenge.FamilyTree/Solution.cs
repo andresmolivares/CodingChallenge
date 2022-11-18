4f45560ec89efcea076da5de299621897c8ba0e3
Andres <andresmolivares@hotmail.com>
2022-11-18 08:04:53 -0500

    Initial draft Implemented support for GetBirthMonth Addressed iss with data generation in if_the_person_exists_in_tree_the_result_should_be_their_birthday

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace CodingChallenge.FamilyTree
 {
@@ -6,7 +9,40 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            var descendant = FindPerson(person, descendantName);
+            return descendant != default ? descendant.Birthday.ToString("MMMM", CultureInfo.InvariantCulture) : string.Empty;
+        }
+
+        public Person FindPerson(Person person, string descendantName)
+        {
+            return person.Name == descendantName
+                    ? person
+                    : person.Descendants.FirstOrDefaultRecursiveScan(p => p.Descendants, p => p.Name == descendantName);
+        }
+
+    }
+
+    public static class IEnumerableExtensions
+    {
+        public static T FirstOrDefaultRecursiveScan<T>(this IEnumerable<T> owner, 
+            Func<T, IEnumerable<T>> childrenSelector,
+            Predicate<T> condition)
+        {
+            // Handle no items in collection
+            if (owner == null || !owner.Any())
+            {
+                return default;
+            }
+            // When found return item
+            var result = owner.FirstOrDefault(t => condition(t));
+            if (!Equals(result, default(T)))
+            {
+                return result;
+            }
+            // Call recursively
+            return owner
+                .SelectMany(childrenSelector)
+                .FirstOrDefaultRecursiveScan(childrenSelector, condition);
         }
     }
 }
